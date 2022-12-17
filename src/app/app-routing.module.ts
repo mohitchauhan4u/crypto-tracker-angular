e24af3950c0e2e2ebc851d56ef9b9fd6616f2a04
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-12-17 21:38:31 +0530

    added banner image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CoinDetailComponent } from './views/coin-detail/coin-detail.component'
+import { CoinListComponent } from './views/coin-list/coin-list.component'
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'coin-list', pathMatch: 'full' },
+  { path: 'coin-list', component: CoinListComponent },
+  { path: 'coin-detail', component: CoinDetailComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
